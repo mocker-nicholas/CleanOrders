4dd78d24b8da33ea6423cbe628b3c614dc051939
Nicholas Orth <nicholas.r.orth@gmail.com>
2022-11-05 10:11:30 -0500

    Add Entities for accounts

---
+++ OrdersDomain.Core/Aggregates/Entities/Accounts/Account.cs
@@ -0,0 +1,14 @@
+﻿using OrdersDomain.Core.Aggregates.Entities.Bases;
+using OrdersDomain.Core.Interfaces;
+
+namespace OrdersDomain.Core.Aggregates.Entities.Accounts
+{
+    public class Account : BaseEntity, IDateCreateable, IAuditable
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public DateTime DateCreated { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public DateTime DateModified { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}

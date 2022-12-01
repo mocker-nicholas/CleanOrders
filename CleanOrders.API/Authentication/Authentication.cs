8be3f0b16acc515539c720331bb66162e25e01ac
Nicholas Orth <nicholas.r.orth@gmail.com>
2022-11-30 18:34:26 -0600

    Attempting authentication interface

---
+++ CleanOrders.API/Authentication/Authentication.cs
@@ -0,0 +1,68 @@
+﻿using CleanOrders.Infrastructure.Data;
+using Microsoft.IdentityModel.Tokens;
+using OrdersDomain.Core.Aggregates.Entities.Users;
+using OrdersDomain.Core.Interfaces;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace CleanOrders.API.Authentication
+{
+    public class Authentication : IAuthentication
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ApplicationContext _context;
+        public Authentication(IConfiguration configuration, ApplicationContext context)
+        {
+            _configuration = configuration;
+            _context = context;
+        }
+        public string Generate(User user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Email),
+                new Claim(ClaimTypes.Actor, user.Id),
+                new Claim(ClaimTypes.Role, user.RoleId.ToString()),
+            };
+            var token = new JwtSecurityToken(
+                _configuration["Jwt:Issuer"],
+                _configuration["Jwt:Audience"],
+                claims,
+                notBefore: DateTime.Now,
+                expires: DateTime.Now.AddMinutes(60),
+                signingCredentials: credentials
+           );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public User Authenticate(UserLogin userLogin)
+        {
+            var user = _context.Users.FirstOrDefault(x => x.Email == userLogin.Email && x.Password == userLogin.Password);
+            if (user == null)
+            {
+                return null;
+            }
+            return user;
+        }
+
+        public LoggedInUser GetCurrentUser(HttpContext httpContext)
+        {
+            var identity = httpContext.User.Identity as ClaimsIdentity;
+            if (identity != null)
+            {
+                var userClaims = identity.Claims;
+                return new LoggedInUser
+                (
+                    userClaims.FirstOrDefault(u => u.Type == ClaimTypes.Actor).Value,
+                    userClaims.FirstOrDefault(u => u.Type == ClaimTypes.NameIdentifier).Value,
+                    userClaims.FirstOrDefault(u => u.Type == ClaimTypes.Role).Value
+                );
+            }
+            return null;
+        }
+    }
+}

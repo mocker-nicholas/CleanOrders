e1fbead6b6df9f501faba45175fa1268b53416d9
Nicholas Orth <nicholas.r.orth@gmail.com>
2023-01-18 20:15:01 -0600

    Add ApiOrderDto

---
+++ CleanOrders.API/ApiDtos/Orders/ApiOrderDto.cs
@@ -0,0 +1,14 @@
+﻿using OrdersDomain.Core.Aggregates.Entities.Orders;
+
+namespace CleanOrders.API.ApiDtos.Orders
+{
+    public class ApiOrderDto
+    {
+        public string AccountId { get; set; }
+        public decimal Total { get; set; }
+        public DateTime DateCreated { get; set; }
+        public OrderAddress BusinessAddress { get; set; }
+        public OrderAddress ShippingAddress { get; set; }
+        public List<LineItem> LineItems { get; set; }
+    }
+}

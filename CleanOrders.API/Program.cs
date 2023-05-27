089430e5c078baaca1a4e8f3d0e9dc38db010442
Nicholas Orth <nicholas.r.orth@gmail.com>
2023-05-26 19:18:52 -0500

    add connection for docker container db

--- CleanOrders.API/Program.cs
+++ CleanOrders.API/Program.cs
@@ -84,6 +84,12 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationContext>();
+    db.Database.Migrate();
+}
+
 app.Run();
 
 // To Do
